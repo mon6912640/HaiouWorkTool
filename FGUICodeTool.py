4a62951835b0b3e98904c1074ab76dc2997394d1
mon6912640 <010monkey@163.com>
2019-07-30 15:51:27 +0800

    增加for循环替换字符串的形式

--- FGUICodeTool.py
+++ FGUICodeTool.py
@@ -1,14 +1,17 @@
 import argparse
 import re
 from pathlib import Path
+import shutil
 
 
 def export(p_source, p_export, p_all_in_one=False):
     path_source = Path(p_source)
     path_export = Path(p_export)
+    if path_export.exists():
+        shutil.rmtree(str(path_export))
     list_file = sorted(path_source.rglob('*.ts'))
     for v in list_file:
-        with v.open() as f:
+        with v.open(encoding='utf-8') as f:
             str_code = f.read()
             if 'Binder.ts' in v.name:
                 # 处理Binder类
@@ -23,12 +26,10 @@ def export(p_source, p_export, p_all_in_one=False):
 
                 output_str = re.sub('(bindAll\(\):void {\s*$)(.+?)(^\s*}\s*$)', rpl_bind_func, str_code,
                                     flags=re.M | re.DOTALL)
-                path_target = path_export / v.relative_to(path_source)
-                path_target.parent.mkdir(parents=True, exist_ok=True)
-                path_target.write_text(output_str)
             else:
                 # 处理ui导出类
-                def rpl_construct_func(m):
+                # 替换形式1
+                def rpl_construct_func1(m):
                     result = ''
                     result += '\t\t\tlet s = this;\n'
                     s1 = m.group(1)
@@ -37,11 +38,34 @@ def export(p_source, p_export, p_all_in_one=False):
                     result += s2.replace('this', 's')
                     return s1 + '\n' + result + s3
 
-                output_str = re.sub('(super\.constructFromXML\(xml\);)(.+?)(^\s*}\s*$)', rpl_construct_func, str_code,
+                # 替换形式2
+                def rpl_construct_func2(m):
+                    get_str_map = {}
+                    result = ''
+                    s1 = m.group(1)
+                    s2 = m.group(2).lstrip('\n')
+                    s3 = m.group(3)
+                    find_list = re.findall('this\.get([^\\\(]+?)\([\'\"](.+?)[\'\"]\)', s2, flags=re.M)
+                    for v in find_list:
+                        if v[0] not in get_str_map:
+                            get_str_map[v[0]] = []
+                        get_str_map[v[0]].append(v[1])
+                    for k, v in get_str_map.items():
+                        list_name = k
+                        list_str = ''
+                        for pname in v:
+                            list_str += '"{0}",'.format(pname)
+                        result += '\n\t\t\tlet {0} = [{1}];\n' \
+                                  '\t\t\tfor(let i = 0; i<{0}.length; i++)\n' \
+                                  '\t\t\t\tthis[{0}[i]] = <any>this.get{0}({0}[i]);\n'.format(list_name, list_str)
+                    return s1 + '\n' + result + s3
+
+                output_str = re.sub('(super\.constructFromXML\(xml\);)(.+?)(^\s*}\s*$)', rpl_construct_func2, str_code,
                                     flags=re.M | re.DOTALL)
-                path_target = path_export / v.relative_to(path_source)
-                path_target.parent.mkdir(parents=True, exist_ok=True)
-                path_target.write_text(output_str)
+
+            path_target = path_export / v.relative_to(path_source)
+            path_target.parent.mkdir(parents=True, exist_ok=True)
+            path_target.write_text(output_str, encoding='utf-8')
     print('输出完毕')
 
 
---
+++ runexe.cmd
@@ -0,0 +1,3 @@
+@echo off
+FGUICodeTool.exe --source D:/work_haiou/client/sanguoUI/fabu --output D:/work_haiou/client/sanguoUI/fabu2
+pause
