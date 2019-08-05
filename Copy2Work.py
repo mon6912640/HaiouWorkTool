d41c90efd32169079bbdf8e054c3e479e8e35983
mon6912640 <010monkey@163.com>
2019-08-05 17:35:34 +0800

    Copy2Work增加输出指定包功能

--- Copy2Work.py
+++ Copy2Work.py
@@ -6,6 +6,7 @@ from pathlib import Path
 FairyGUI发布文件批量复制脚本
 """
 
+
 class PathVo():
     name = ''
     path_parent: Path = None
@@ -15,16 +16,25 @@ class PathVo():
         self.path_parent = p_path_parent
 
 
-default_work_path = 'D:/work_haiou/client/sanguo/resource/UI'
 default_fabu_path = 'D:/work_haiou/client/sanguoUI/fabu'
+default_work_path = 'D:/work_haiou/client/sanguo/resource/UI'
 
 
-def run(p_work_path, p_fabu_path):
+def run(p_fabu_path, p_work_path, p_pkg_list=None):
     path_work = Path(p_work_path)
     path_fabu = Path(p_fabu_path)
     if not path_fabu.exists():
         print('发布目录不存在，程序退出')
         return
+
+    if p_pkg_list is None or len(p_pkg_list) == 0:
+        is_all = True
+        print('本次将输出全部包')
+    else:
+        is_all = False
+        print('本次将输出{0}个包'.format(len(p_pkg_list)), p_pkg_list)
+
+    total_count = 0
 
     path_map = {}
     list_file = sorted(path_work.rglob('*.*'))
@@ -34,33 +44,49 @@ def run(p_work_path, p_fabu_path):
 
     list_file = sorted(path_fabu.rglob('*.*'))
     for v in list_file:
+        if v.is_dir():
+            continue
         path_target = None
+        handle_flag = False
         if v.suffix == '.bin':
+            pkg = v.name.split('.')[0]
+            if not is_all and pkg not in p_pkg_list:
+                continue
             if v.name in path_map:
                 path_target = path_map[v.name].path_parent / v.name
             else:
-                pkg = v.name.split('.')[0]
                 path_target = path_work / pkg / v.name
                 if not path_target.parent.exists():
                     path_target.parent.mkdir(parents=True, exist_ok=True)
-            shutil.copy(str(v), str(path_target))
+            handle_flag = True
         elif '_atlas' in v.name:
+            pkg = v.name.split('_')[0]
+            if not is_all and pkg not in p_pkg_list:
+                continue
             if v.name in path_map:
                 path_target = path_map[v.name].path_parent / v.name
             else:
-                pkg = v.name.split('_')[0]
                 path_target = path_work / pkg / v.name
                 if not path_target.parent.exists():
                     path_target.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copy(str(v), str(path_target))
+            handle_flag = True
+        if handle_flag:
+            shutil.copy(str(v), str(path_target))
+            total_count += 1
+            # print('{0}\n-->复制到{1}'.format(str(v), str(path_target)))
 
-    print('成功复制bin和atlas图集到目录{0}'.format(str(path_work)))
+    print('成功复制bin和atlas图集到目录{0}，共{1}个文件'.format(str(path_work), total_count))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='帮助信息')
     parser.add_argument('--source', type=str, default='./fabu', help='fui工程目录')
     parser.add_argument('--to', type=str, default='./fabu2', help='输出的目录')
+    parser.add_argument('--pkg', type=str, default='common,Share', help='指定的包名称列表，用逗号分隔，默认为空则为输出所有包')
     args = parser.parse_args()
 
-    run(args.source, args.to)
+    pkg_list = []
+    if args.pkg:
+        pkg_list = args.pkg.split(',')
+
+    run(args.source, args.to, pkg_list)
---
+++ run_Copy2Work.cmd
@@ -0,0 +1,3 @@
+@echo off
+python Copy2Work.py --pkg common,Share --source D:/work_haiou/client/sanguoUI/fabu --to D:/work_haiou/client/sanguo/resource/UI
+pause
