94f93fb7c702a67d7307c0d313bc2b8e289d89b4
mon6912640 <010monkey@163.com>
2019-08-19 12:05:22 +0800

    修复pyinstaller打包错误

--- publish.py
+++ publish.py
@@ -2,6 +2,7 @@ import Copy2Work
 import Copy2Work2
 import argparse
 from pathlib import Path
+import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='帮助信息')
@@ -13,6 +14,34 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    if not args.fgui:
+        print('请指定fgui的工程目录')
+        sys.exit()
+
+    if not args.code:
+        print('请指定代码工程目录')
+        sys.exit()
+
+    path_code = Path(args.code)
+    if not path_code.exists():
+        print('代码工程目录不存在')
+        sys.exit()
+
+    file_egret = path_code / 'egretProperties.json'
+    if not file_egret.exists():
+        print('指定的代码工程目录非Egret工程')
+        sys.exit()
+
+    path_fgui = Path(args.fgui)
+    if not path_fgui.exists():
+        print('fgui工程目录不存在')
+        sys.exit()
+
+    file_fgui = path_fgui / 'sanguoUI.fairy'
+    if not file_fgui.exists():
+        print('指定的fgui工程目录错误')
+        sys.exit()
+
     pkg_list = []
     if args.type == 1:  # 等待用户输入
         input_pkg = input('输入你要发布的包名，多个包可用逗号分隔，不填则为输出所有包:')
@@ -22,34 +51,6 @@ if __name__ == '__main__':
     else:  # 参数运行
         if args.pkg:
             pkg_list = args.pkg.split(',')
-
-    if not args.fgui:
-        print('请指定fgui的工程目录')
-        exit()
-
-    if not args.code:
-        print('请指定代码工程目录')
-        exit()
-
-    path_code = Path(args.code)
-    if not path_code.exists():
-        print('代码工程目录不存在')
-        exit()
-
-    file_egret = path_code / 'egretProperties.json'
-    if not file_egret.exists():
-        print('指定的代码工程目录非Egret工程')
-        exit()
-
-    path_fgui = Path(args.fgui)
-    if not path_fgui.exists():
-        print('fgui工程目录不存在')
-        exit()
-
-    file_fgui = path_fgui / 'sanguoUI.fairy'
-    if not file_fgui.exists():
-        print('指定的fgui工程目录错误')
-        exit()
 
     Copy2Work.run(str(path_fgui / 'fabu'), str(path_code / 'resource' / 'UI'), pkg_list)
     Copy2Work2.run(str(path_fgui / 'fabu'), str(path_code / 'src'), str(path_fgui / 'assets'), pkg_list)
